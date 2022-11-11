f161462a6459b5393580d4f92ccc52e3124812b5
Alex Axthelm <aaxthelm@RMI.org>
2022-11-11 17:21:59 +0000

    keep only docker-specific changes

--- meta_report_data_creator/phase-0_install.R
+++
@@ -1,3 +0,0 @@
-devtools::install_local("/datadrive/pacta-runner/pacta.portfolio.analysis", dependencies = TRUE)
-# devtools::install_local("/datadrive/pacta-runner/pacta.interactive.report", dependencies = TRUE)
-devtools::install_local("/datadrive/pacta-runner/pacta.executive.summary", dependencies = TRUE)

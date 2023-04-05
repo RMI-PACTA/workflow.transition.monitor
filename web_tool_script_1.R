RMI-PACTA/workflow.transition.monitor

commit ed6b11231e9e3d1f2b5512ab39b215a4fd3a7291

remove adding of revenue data (#142)

web_tool_script_1.R
@@ -103,8 +103,6 @@ portfolio <- process_raw_portfolio(
 # # list ISINs of unknown funds in funds. the list includes value_usd to estimate importance of the isin for o
 # unknown_funds_in_funds <- list_unknown_funds_in_funds(portfolio)
 
-portfolio <- add_revenue_split(has_revenue, portfolio, revenue_data)
-
 portfolio <- create_ald_flag(portfolio, comp_fin_data = abcd_flags_equity, debt_fin_data = abcd_flags_bonds)
 
 eq_portfolio <- create_portfolio_subset(

commit e5d73a83b59635ab626986ddcd20a649cba7243a

fix bug introduced by #142
#142 introduced a bug because `pacta.portfolio.analysis::add_revenue_split()` had a unknown behavior of mapping the `security_mapped_sector` column of the portfolio data to `financial_sector`, which is then needed later on.

This is not a pretty solution (probably we should just rename the column from the start in data.prep eventually), but it gets the job done exactly where it used to.

As reference, we need to replicate the behavior here: https://github.com/RMI-PACTA/pacta.portfolio.analysis/blob/b7f837d0e19774fc8e81570ab7c0bc395788a595/R/add_revenue_split.R#L50-L56

web_tool_script_1.R
@@ -81,6 +81,13 @@ portfolio <- process_raw_portfolio(
   isin_to_fund_table = isin_to_fund_table
 )
 
+portfolio <-
+  portfolio %>%
+  mutate(
+    has_revenue_data = FALSE,
+    financial_sector = .data$security_mapped_sector
+  )
+
 portfolio <- create_ald_flag(portfolio, comp_fin_data = abcd_flags_equity, debt_fin_data = abcd_flags_bonds)
 
 eq_portfolio <- create_portfolio_subset(
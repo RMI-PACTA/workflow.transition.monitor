a2a9d4164ba6b887fd7128d87d35b7bc8cb25ccf
Jackson Hoffart <jackson.hoffart@gmail.com>
2023-05-05 14:07:02 +0200

    Check that dataset exists before trying to add columns (#183)

--- web_tool_script_3.R
+++ web_tool_script_3.R
@@ -58,6 +58,10 @@ readRDS_or_return_alt_data <- function(filepath, alt_return = NULL) {
 }
 
 add_inv_and_port_names_if_needed <- function(data) {
+  if (!inherits(data, "data.frame")) {
+    return(data)
+  }
+
   if (!"portfolio_name" %in% names(data)) {
     data <- mutate(data, portfolio_name = .env$portfolio_name, .before = everything())
   }
